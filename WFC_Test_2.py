5ac683bf847842bfe873ef81790002fcb3efb9e1
Mystrite <finleydenton@outlook.com>
2023-05-31 19:35:37 +0100

    Rewrote unblocking algorithm to be a seperate function, updated some general py file formatting, mostly happy with this version now. Changed name of particular tile types.

--- WFC_Test_2.py
+++ WFC_Test_2.py
@@ -1,16 +1,3 @@
-"""
-TILE ID:
-0 - None
-1 - Floor
-2 - Lava
-3 - Holy Water
-4 - Rock
-5 - New Void
-6 - Pre-set Void
-7 - Exit
-8 - Entrance
-"""
-
 import random as r
 
 ############ VALUE PRESETS BEGIN ############
@@ -20,28 +7,26 @@ TILE_ID = {
     "LAVA": 1,
     "HOLY": 2,
     "ROCK": 3,
-    "N_VOID": 4,
-    "P_VOID": 5,
-    "EXIT": 6,
-    "ENTER": 7,
-    "NONE": 8,
-    "O_WALL": 9
+    "VOID": 4,
+    "EXIT": 5,
+    "ENTER": 6,
+    "NONE": 7,
+    "O_WALL": 8
 }
 
 SIZE_X = 18
 SIZE_Y = 8
 prob_matrix = [
-   # FLOOR      LAVA        HOLY        ROCK        N_VOID      P_VOID  EXIT    ENTER   NONE    O_WALL     
-    [0.70,      0.1,        0.005,      0.14,       0.028,      0,      0,      0,      0,      0],         # FLOOR
-    [0.50,      0.45,       0,          0.05,       0,          0,      0,      0,      0,      0],         # LAVA
-    [0.65,      0,          0.3,        0.05,       0,          0,      0,      0,      0,      0],         # HOLY
-    [0.75,      0.10,       0.05,       0.05,       0.05,       0,      0,      0,      0,      0],         # ROCK
-    [0.70,      0,          0.005,      0.005,      0.2,        0,      0,      0,      0,      0],         # N_VOID
-    [0.70,      0,          0,          0,          0.3,        0,      0,      0,      0,      0],         # P_VOID
-    [1,         0,          0,          0,          0,          0,      0,      0,      0,      0],         # EXIT
-    [1,         0,          0,          0,          0,          0,      0,      0,      0,      0],         # ENTRANCE
-    [0.90,      0.03,       0.005,      0.04,       0.025,      0,      0,      0,      0,      0],         # NONE
-    [0,         0,          0,          0,          0,          0,      0,      0,      0,      0]          # O_WALL
+   # FLOOR      LAVA        HOLY        ROCK        VOID       EXIT    ENTER   NONE    O_WALL     
+    [0.7,       0.1,        0.005,      0.14,       0.028,     0,      0,      0,      0],         # FLOOR
+    [0.5,       0.45,       0,          0.05,       0,         0,      0,      0,      0],         # LAVA
+    [0.65,      0,          0.3,        0.05,       0,         0,      0,      0,      0],         # HOLY
+    [0.75,      0.1,        0.05,       0.05,       0.05,      0,      0,      0,      0],         # ROCK
+    [0.6,       0,          0.005,      0.005,      0.3,       0,      0,      0,      0],         # VOID
+    [1,         0,          0,          0,          0,         0,      0,      0,      0],         # EXIT
+    [1,         0,          0,          0,          0,         0,      0,      0,      0],         # ENTRANCE
+    [0.9,       0.03,       0.005,      0.04,       0.025,     0,      0,      0,      0],         # NONE
+    [0,         0,          0,          0,          0,         0,      0,      0,      0]          # O_WALL
     ]
 
 ############ VALUE PRESETS END ############
@@ -64,14 +49,16 @@ def init_grid(xlen, ylen):      #create 2d array which holds map tiles
     maptiles = [[TILE_ID["NONE"]] * xlen for i in range(ylen)]
     for x in range(xlen):
         for y in range(ylen):
-            if y == 0 and x == exit_index:
+
+            if y == 0 and x == exit_index:      # preset entrances and exit points
                 maptiles[0][exit_index] = TILE_ID["EXIT"]
             elif y == ylen-1 and x == enter_index:
                 maptiles[y][enter_index] = TILE_ID["ENTER"]
-                maptiles[y-1][enter_index] = TILE_ID["FLOOR"]       # ensures entrance is not blocked by a non-floor tile
-            elif (y == 0 or y == ylen-1) or (x == 0 or x == xlen-1):
+
+            elif (y == 0 or y == ylen-1) or (x == 0 or x == xlen-1):        # border map with wall tiles
                 maptiles[y][x] = TILE_ID["O_WALL"]
-    return maptiles, exit_index
+
+    return maptiles
 
 
 def rand_select(matrix, adj_ID):        #select a tile type, given a tile next to it and a probability matrix
@@ -90,10 +77,8 @@ def rand_select(matrix, adj_ID):        #select a tile type, given a tile next t
     return selected_ID
      
 def populate_grid(grid, x, y, ID):      #populate given grid
-    outgrid(grid, SIZE_X, SIZE_Y)
-
     if (x < SIZE_X and y < SIZE_Y and x >= 0 and y >= 0):
-       if (grid[y][x] == TILE_ID["NONE"]):  # mostly generates horizontally, not a huge fan
+       if grid[y][x] == TILE_ID["NONE"]:  # mostly generates horizontally, not a huge fan
         # generate tile
            newID = rand_select(prob_matrix, ID)
            grid[y][x] = newID
@@ -102,15 +87,26 @@ def populate_grid(grid, x, y, ID):      #populate given grid
            grid = populate_grid(grid,x-1, y, newID)
            grid = populate_grid(grid,x, y-1, newID)
 
-       elif grid[y][x] == TILE_ID["EXIT"]:      # ensure that exit is not directly blocked by non-floor tile
-           newID = TILE_ID["FLOOR"]
-           grid[y-1][x] = newID
-           grid = populate_grid(grid,x, y+1, newID)
-           grid = populate_grid(grid,x+1, y, newID)
-           grid = populate_grid(grid,x-1, y, newID)
-           grid = populate_grid(grid,x, y-1, newID)
-
     return grid
+
+def unblock_map(grid):      # ensures exits and entrances are not directly blocked by non-floor tiles
+    for x in range(SIZE_X):
+        if grid[0][x] == TILE_ID["EXIT"]:
+            grid[1][x] = TILE_ID["FLOOR"]
+        if grid[SIZE_Y-1][x] == TILE_ID["ENTER"]:
+            grid[SIZE_Y-2][x] = TILE_ID["FLOOR"]
+    
+    return grid
+    
+def create_maplist():       # creates the list of maps which will be used for the game
+    num_maps = r.randint(3, 5)
+    maplist = [None] * num_maps
+    print("Generating %s maps...\n" % num_maps)
+    for i in range(num_maps):
+        maptiles = init_grid(SIZE_X, SIZE_Y)        # to be rewritten, mostly output test            
+        maplist[i] = unblock_map(populate_grid(maptiles, 1, 1, TILE_ID["EXIT"]))   # populates from exit
+        
+    return maplist, num_maps
 
 ############ MAP GEN FUNCS END ############
 
@@ -120,11 +116,12 @@ def main():
     global SIZE_Y
     global prob_matrix
 
-    maptiles, n_index = init_grid(SIZE_X, SIZE_Y)        # to be rewritten, mostly output test
-    outgrid(maptiles, SIZE_X, SIZE_Y)             
-    newgrid = populate_grid(maptiles, n_index, 0, 8)
-    print()
-    outgrid(newgrid, SIZE_X, SIZE_Y)
+    maplist, num_maps = create_maplist()
+    for i in range(num_maps):
+        print("MAP %s:\n" % (i+1))
+        outgrid(maplist[i], SIZE_X, SIZE_Y)
+        print()
 
 main()
+
 
