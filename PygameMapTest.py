516a61270bd31059c2ad8b0867b29f3fa56494de
Mystrite <FinleyDenton@outlook.com>
2023-06-22 15:30:58 +0100

    Further changes to pygamegamemaptest.py

--- PygameMapTest.py
+++ PygameMapTest.py
@@ -1,26 +1,33 @@
 import pygame
 
+SWIDTH = 1440
+SHEIGHT = 720
+SCREEN = (SWIDTH, SHEIGHT)
+
 x = 50
 y = 50
-width = 40
-height = 60
-vel = 5
+vel = 15
 
 run = True
 
 pygame.init()
 
-win = pygame.display.set_mode((1440, 720))
+win = pygame.display.set_mode(SCREEN)
 player_icon = pygame.image.load('./A-Level-NEA-new/assets/PLAYER_placeholder.png')
+player_icon = pygame.transform.scale(player_icon, (64 , 64))
 bg = pygame.image.load('./A-Level-NEA-new/assets/BACKGROUND_placeholder.png')
 pygame.display.set_caption("very cool epic game for cool people")
 
+clock = pygame.time.Clock()
+
+
 def redraw():
     win.blit(bg, (0,0))
+    win.blit(player_icon, (x,y))
     pygame.display.update()
 
 while run:
-    pygame.time.delay(100)
+    clock.tick(30)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -40,7 +47,6 @@ while run:
         y += vel
 
     win.fill((255, 255, 255))
-    pygame.draw.rect(win, (255,0,0), (x, y, width, height))
     redraw()
 
 
